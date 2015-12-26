c6e13c86d533ba8e9604d43b0e136bfe428947cd
Jordan Hudgens <jordan@wow.com>
2015-12-26 14:01:59 -0600

    Integrated changes

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -12,14 +12,14 @@ class SchoolClassesController < ApplicationController
   end
 
   def create
-    @school_class = SchoolClass.new(params.require(:school_class).permit(:title, :room_number))
+    @school_class = SchoolClass.new(params.require(:school_class))
     @school_class.save
     redirect_to school_class_path(@school_class)
   end
 
   def update
     @school_class = SchoolClass.find(params[:id])
-    @school_class.update(params.require(:school_class).permit(:title, :room_number))
+    @school_class.update(params.require(:school_class))
     redirect_to school_class_path(@school_class)
   end
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,14 +12,14 @@ class StudentsController < ApplicationController
   end
 
   def create
-    @student = Student.new(params.require(:student).permit(:first_name, :last_name))
+    @student = Student.new(params.require(:student))
     @student.save
     redirect_to student_path(@student)
   end
 
   def update
     @student = Student.find(params[:id])
-    @student.update(params.require(:student).permit(:first_name, :last_name))
+    @student.update(params.require(:student))
     redirect_to student_path(@student)
   end
 end
