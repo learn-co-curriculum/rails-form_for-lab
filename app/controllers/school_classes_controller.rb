14726075b709e8f7eb872d93124062d658cc94f5
maxwellbenton <maxwellbenton@gmail.com>
2019-02-21 16:32:25 -0500

    updates controller solutions with permitted params

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -12,14 +12,20 @@ class SchoolClassesController < ApplicationController
   end
 
   def create
-    @school_class = SchoolClass.new(params.require(:school_class))
+    @school_class = SchoolClass.new(school_class_params)
     @school_class.save
     redirect_to school_class_path(@school_class)
   end
 
   def update
     @school_class = SchoolClass.find(params[:id])
-    @school_class.update(params.require(:school_class))
+    @school_class.update(school_class_params)
     redirect_to school_class_path(@school_class)
+  end
+
+  private
+
+  def school_class_params
+    params.require(:school_class).permit!
   end
 end
