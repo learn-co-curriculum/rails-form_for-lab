65125f9dd0a4d06fd5af94ec8eb2e0c3a83f438a
maxwellbenton <maxwellbenton@gmail.com>
2019-03-07 11:38:00 -0500

    merge with wip-rails-update-solution

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -12,7 +12,7 @@ class SchoolClassesController < ApplicationController
   end
 
   def create
-    @school_class = SchoolClass.new(params.require(:school_class).permit(:title, :room_number))
+    @school_class = SchoolClass.new(school_class_params)
     @school_class.save
     redirect_to school_class_path(@school_class)
   end
