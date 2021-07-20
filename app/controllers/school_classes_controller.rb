e61f3f91bb14dbad954e7334f68d2aa0679cd4a7
drewsview34 <drewsview34@outlook.com>
2021-07-20 11:08:37 -0700

    created controller files

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,23 @@
+class SchoolClassesController < ApplicationController
+
+    def new
+
+    end
+
+    def create
+
+    end
+
+    def show
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+end
---
+++ app/controllers/students.rb
@@ -0,0 +1,23 @@
+class StudentsController < ApplicationController
+
+    def new
+
+    end
+
+    def create
+
+    end
+
+    def show
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+end
